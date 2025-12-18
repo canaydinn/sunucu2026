f0263825c7d99096dd750d14780abdf1a28cf2fa
Can Aydın <canaydinn@gmail.com>
2025-12-18 15:08:51 +0300

    satislar hazır

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+const path=require('path');
 const app=express();
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
 port=process.env.PORT || 3001;
 const router=require("./routers")
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 //localhost:3000/api ye gelen bütün istekleri routers klasöründeki index.js dosyasına yönlendir
 app.use("/api",router);
--- routers/routes.js
+++ routers/routes.js
@@ -1,5 +1,7 @@
 const router=require('express').Router();
-const {kullanici_login,kullanici_ekle}=require("../controllers/controller")
+const {kullanici_login,kullanici_ekle,kullanici_getir,satis_getir}=require("../controllers/controller")
 router.post('/login',kullanici_login)
 router.post('/register',kullanici_ekle)
+router.get('/users',kullanici_getir)
+router.get('/sales',satis_getir)
 module.exports=router;
